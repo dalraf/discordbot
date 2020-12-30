621d677009cd5221500473b192288ebcdca63215
Daniel Fonseca Alves <dalraf@gmail.com>
2020-12-30 14:17:39 -0300

    Print de status

--- app/start.py
+++ app/start.py
@@ -13,8 +13,10 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='testar', help='Simula ação do jogo: Status Nível Dificuldade')
 async def testar(ctx, status: int , nivel: int , dificuldade: int):
+    print("Recebendo teste de Status=" + str(status) + ", Nível=" + str(nivel) + ", Dificuldade=" + str(dificuldade))
     pontos = randrange(20)
     saldo = ((status - pontos ) * nivel) - dificuldade
+    print("Resultado=" + str(saldo))
     await ctx.send(str(saldo))
 
 bot.run(TOKEN)
