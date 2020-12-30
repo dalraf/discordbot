117f829341176f36aeb1f85c6ee1d16afdad99d2
Daniel Fonseca Alves <dalraf@gmail.com>
2020-12-30 14:26:30 -0300

    Correcao do caracter

--- app/start.py
+++ app/start.py
@@ -14,7 +14,7 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='testar', help='Simula ação do jogo: Status Nível Dificuldade')
 async def testar(ctx, status: int , nivel: int , dificuldade: int):
-    print("Recebendo teste de Status=" + str(status) + ", Nível=" + str(nivel) + ", Dificuldade=" + str(dificuldade))
+    print("Recebendo teste de Status=" + str(status) + ", Nivel=" + str(nivel) + ", Dificuldade=" + str(dificuldade))
     pontos = randrange(20)
     saldo = ((status - pontos ) * nivel) - dificuldade
     print("Resultado=" + str(saldo))
